80480b58db2b12f012042f92bdc6b9ee14d0f181
ijio <ijio@fresnoU.org>
2025-05-27 02:24:02 -0700

    Second Commit

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,4 +1,4 @@
-const {Article} = require('../models')
+const {Article, Comment, Reply, user} = require('../models')
 
 module.exports.renderAddForm = function (req, res) {
     const article = {
@@ -16,14 +16,14 @@ module.exports.addArticle = async function (req, res) {
         intro: req.body.intro,
         image_url: req.body.image_url,
         body: req.body.body,
-        author_id: 1, //todo get logged in user
+        author_id: req.user.id,
         published_on: new Date()
     });
-    res.redirect('/') //todo change the redirect to view all once made
+    res.redirect('/')
 };
 
 module.exports.displayArticle = async function (req, res) {
-    const article = await Article.findByPk(req.params.articleId, {
+    const article = await Article.findByPk(req.params.articleId,{
         include: ['author']
     });
     res.render('articles/view', {article});
@@ -34,4 +34,68 @@ module.exports.displayAll = async function (req, res) {
         include: ['author']
     });
     res.render('articles/viewAll', {articles});
+}
+
+module.exports.renderEditForm = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId);
+    if (!article.isOwnedBy(user.id)){
+        res.redirect('/');
+        return;
+    }
+    res.render('articles/edit', {article});
+};
+
+module.exports.updateArticle = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId);
+    if (!article.isOwnedBy(user.id)){
+        res.redirect('/');
+        return;
+    }
+    await Article.update({
+        title: req.body.title,
+        intro: req.body.intro,
+        image_url: req.body.image_url,
+        body: req.body.body,
+    }, {
+        where: {
+            id: req.params.articleId
+        }
+    });
+    res.redirect(`/article/${req.params.articleId}`);
+}
+
+module.exports.deleteArticle = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId);
+    if (!user.is('admin') && !article.isOwnedBy(user)){
+        res.redirect('/');
+        return;
+    }
+    await Article.destroy({
+        where: {
+            id: req.params.articleId
+        }
+    });
+    res.redirect('/')
+};
+
+module.exports.displayArticle = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId, {
+        include: [
+            'author',
+            {
+                model: Comment,
+                as: 'comments',
+                required: false,
+                include: [{
+                    model: Reply,
+                    as: 'replies',
+                    required: false
+                }]
+            }
+        ],
+        order: [
+            ['comments', 'commented_on', 'desc']
+        ]
+    });
+    res.render('articles/view', {article});
 }
